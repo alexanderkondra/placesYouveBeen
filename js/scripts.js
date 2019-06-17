6447027549490359616cedd65015adf2d8944c8e
Alexander Kondra and Kira Roberts <alexander@>
2019-06-17 16:11:04 -0700

    add styles, edit index, edit scripts

---
+++ js/scripts.js
@@ -0,0 +1,20 @@
+$(document).ready(function() {
+  $("#phuketThailand").click(function() {
+    $("#phuketThailandShow").toggle();
+  });
+  $("#romeItaly").click(function() {
+    $("#romeItalyShow").toggle();
+  });
+  $("#moscowRussia").click(function() {
+    $("#moscowRussiaShow").toggle();
+  });
+  $("#philly").click(function() {
+    $("#phillyShow").toggle();
+  });
+  $("#seattle").click(function() {
+    $("#seattleShow").toggle();
+  });
+  $("#cheyenne").click(function() {
+    $("#cheyenneShow").toggle();
+  });
+});
